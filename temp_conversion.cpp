2d6fe5e3656969ce6a19415937e60fdc12505065
Kate Charbonnet <kchrbnnt@memphis.edu>
2017-02-20 21:40:50 -0600

    Added unit_2

--- temp_conversion.cpp
+++ temp_conversion.cpp
@@ -3,42 +3,43 @@
 using namespace std;
 
 char unit;
+char unit_2;
 float temp;
 float celsius;
 float fahrenheit;
 float kelvin;
 
-float f_to_c()
+int f_to_c()
 {
     celsius = ((fahrenheit - 32) * 5) / 9;
     return 0;
 }
 
-float c_to_f()
+int c_to_f()
 {
     fahrenheit = (((celsius * 9) / 5) + 32);
     return 0;
 }
 
-float k_to_c()
+int k_to_c()
 {
     celsius = (kelvin - 273.15);
     return 0;
 }
 
-float c_to_k()
+int c_to_k()
 {
     kelvin = (celsius + 273.15);
     return 0;
 }
 
-float k_to_f()
+int k_to_f()
 {
     fahrenheit = ((((kelvin - 273.15)* 9) / 5) + 32);
     return 0;
 }
 
-float f_to_k()
+int f_to_k()
 {
     kelvin = ((((fahrenheit - 32) * 5) / 9)+273.15);
     return 0;
@@ -49,25 +50,54 @@ int main()
     cout << "Enter a temperature with units: ";
     cin >> temp;
     cin >> unit;
+	
+    cout << "What temperature unit would you like to convert to?";
+    cin >> unit_2;
     
-    if(isupper(unit))
+    if(isupper(unit))  //converts uppercase units to lowercase
 	{
 		unit = tolower(unit);
 	}
     
-    if(unit == 'f')
+    if(isupper(unit_2))  //converts uppercase units to lowercase
+	{
+		unit = tolower(unit_2);
+	}	
+	
+    if(unit == 'f' && unit_2 == 'c')
     {    
-        cout << f_to_c "degrees C\n" && f_to_k "Kelvin";
+        temp = f_to_c;
+	cout << temp << "degrees Celsius\n";
     }
     
-    if(unit == 'c')
+    if(unit == 'f' && unit_2 == 'k')
     {
-        cout << c_to_f "degrees F\n" && c_to_k "Kelvin";
+        temp = f_to_k;
+	cout << temp << "Kelvin\n";
     }
     
-    if(unit == 'k')
+    if(unit == 'c'  && unit_2 == 'f')
     {
-        cout << k_to_f "degrees F\n" && k_to_c "degrees C";
+        temp = c_to_f;
+	cout << temp << "degrees Fahrenheit\n";
+    }
+	
+    if(unit == 'c' && unit_2 == 'k')
+    {
+        temp = c_to_k;
+	cout << temp << "Kelvin\n";
+    }
+
+    if(unit == 'k' && unit_2 == 'f')
+    {
+        temp = k_to_f;
+	cout << temp << "degrees Fahrenheit\n";
+    }
+	
+    if(unit == 'k' && unit_2 == 'c')
+    {
+        temp = k_to_c;
+	cout << temp << "degrees Celsius\n";
     }
     return 0;
 }
