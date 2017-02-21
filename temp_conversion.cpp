523d2cb0b2f42e576494987f9dd5fbcaf4a72007
Kate Charbonnet <kchrbnnt@memphis.edu>
2017-02-20 22:00:44 -0600

    Spaces before "degrees"

--- temp_conversion.cpp
+++ temp_conversion.cpp
@@ -22,43 +22,43 @@ int main()
     
     if(isupper(unit_2))  //converts uppercase units to lowercase
 	{
-		unit = tolower(unit_2);
+		unit_2 = tolower(unit_2);
 	}	
 	
     if(unit == 'f' && unit_2 == 'c')
     {    
         temp = ((temp - 32) * 5) / 9;
-	cout << temp << "degrees Celsius\n";
+	cout << temp << " degrees Celsius\n";
     }
     
     if(unit == 'f' && unit_2 == 'k')
     {
         temp = ((((temp - 32) * 5) / 9)+273.15);
-	cout << temp << "Kelvin\n";
+	cout << temp << " Kelvin\n";
     }
     
     if(unit == 'c'  && unit_2 == 'f')
     {
         temp = (((temp * 9) / 5) + 32);
-	cout << temp << "degrees Fahrenheit\n";
+	cout << temp << " degrees Fahrenheit\n";
     }
 	
     if(unit == 'c' && unit_2 == 'k')
     {
         temp = (temp + 273.15);
-	cout << temp << "Kelvin\n";
+	cout << temp << " Kelvin\n";
     }
 
     if(unit == 'k' && unit_2 == 'f')
     {
         temp = ((((temp - 273.15)* 9) / 5) + 32);
-	cout << temp << "degrees Fahrenheit\n";
+	cout << temp << " degrees Fahrenheit\n";
     }
 	
     if(unit == 'k' && unit_2 == 'c')
     {
         temp = (temp - 273.15);
-	cout << temp << "degrees Celsius\n";
+	cout << temp << " degrees Celsius\n";
     }
     return 0;
 }
